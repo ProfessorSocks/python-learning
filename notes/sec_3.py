17cd24c0683634c0c157aec5f5e454a54a1eafaa
ProfessorSocks <124716422+ProfessorSocks@users.noreply.github.com>
2026-01-13 09:10:20 -0800

    commit 1/13/2026
    added notes

---
+++ notes/sec_3.py
@@ -0,0 +1,16 @@
+#store data to use it later
+#ctrl + s to save
+# ctrl + / to add comment or remove
+#python is snake_case
+
+#string
+my_name = "Camille West" 
+print(my_name)
+
+#number
+my_age = 23
+print(my_age)
+
+#boolean
+is_coding = True
+
